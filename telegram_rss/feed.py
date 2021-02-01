85842729bd2b2e41532346f88ecefb4e856ed2bc
hexatester <revolusi147id@gmail.com>
2021-02-01 11:33:20 +0700

    Update .feed

--- telegram_rss/feed.py
+++ telegram_rss/feed.py
@@ -1,9 +1,10 @@
 import attr
 import os
 from feedparser import parse as parse_feed
+from typing import List
 
 from telegram_rss.config import Config, FeedConfig
-from telegram_rss.utils import sanitize_text
+from telegram_rss.utils import sanitize_text, save_as
 
 
 @attr.dataclass
@@ -19,6 +20,11 @@ class Feed:
     def __str__(self):
         return self.title
 
+    def save(self, directory: str):
+        data = attr.asdict(self, recurse=True)
+        filepath = os.path.join(directory, self.title, '.toml')
+        save_as(data, filepath)
+
 
 @attr.dataclass
 class FeedUpdater:
@@ -28,8 +34,9 @@ class FeedUpdater:
     def __call__(self):
         pass
 
-    def get_new_feeds(self):
-        feeds = self._feeds
+    def get_new_feeds(self) -> List[Feed]:
+        feeds: List[Feed] = list()
+        return feeds
 
     def get_feeds(self):
         return parse_feed(self.rss.source)

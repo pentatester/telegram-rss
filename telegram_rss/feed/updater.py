3ae0edc71cee9d875e3459aa23319c094b6d851d
hexatester <revolusi147id@gmail.com>
2021-02-01 16:53:40 +0700

    Improve FeedUpdater

--- telegram_rss/feed/updater.py
+++ telegram_rss/feed/updater.py
@@ -3,31 +3,46 @@ import os
 import toml
 
 from feedparser import parse as parse_feed
-from typing import List
+from typing import List, Optional
 
 from telegram_rss.config import Config, FeedConfig
 from telegram_rss.utils import save_as
-from . import Feed
+from . import Entry, Feed
 
 
 class FeedUpdater:
     def __init__(self, config: Config, feed_config: FeedConfig):
         self.config = config
         self.feed_config = feed_config
+        self._feed: Optional[Feed] = None
+        self._local_feed: Optional[Feed] = None
 
-    def __call__(self) -> List[Feed]:
-        return self.get_feeds()
+    def __call__(self) -> List[Entry]:
+        return self.get_new_entries()
 
-    def get_new_feeds(self) -> List[Feed]:
-        feeds: List[Feed] = list()
-        return feeds
+    def get_new_entries(self) -> List[Entry]:
+        entries: List[Entry] = list()
+        if self.feed == self.local_feed:
+            return entries
+        for feed in self.feed:
+            if feed not in self.local_feed:
+                entries.append(feed)
+        return entries
 
-    def get_feeds(self):
-        return parse_feed(self.feed_config.source)
+    @property
+    def feed(self) -> Feed:
+        if self._feed:
+            return self._feed
+        self._feed = Feed.from_feedparser(parse_feed(self.feed_config.source))
+        return self._feed
 
-    def get_local_feed(self) -> Feed:
+    @property
+    def local_feed(self) -> Feed:
+        if self._local_feed:
+            return self._local_feed
         feed_data = toml.load(self.local_file)
-        return Feed(**feed_data)
+        self._local_feed = Feed(**feed_data)
+        return self._local_feed
 
     def _save_feed(self, feed: Feed):
         feed_data = attr.asdict(feed, recurse=True)

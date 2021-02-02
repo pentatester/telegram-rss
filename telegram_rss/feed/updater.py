df27bbb81d2c79f22415339c1a7391d0fc4c2494
hexatester <revolusi147id@gmail.com>
2021-02-02 09:13:49 +0700

    Fix FeedUpdater

--- telegram_rss/feed/updater.py
+++ telegram_rss/feed/updater.py
@@ -1,32 +1,44 @@
 import attr
+import logging
 import os
 import toml
 
 from feedparser import parse as parse_feed
 from typing import List, Optional
 
-from telegram_rss.config import Config, FeedConfig
-from telegram_rss.utils import save_as
+from telegram_rss.config import FeedConfig
+from telegram_rss.utils import save_as, get_default_directory
 from . import Entry, Feed
 
 
 class FeedUpdater:
-    def __init__(self, config: Config, feed_config: FeedConfig):
-        self.config = config
+    def __init__(self, feed_config: FeedConfig, ext: str = ".toml"):
         self.feed_config = feed_config
         self._feed: Optional[Feed] = None
         self._local_feed: Optional[Feed] = None
+        self.local_file = os.path.join(
+            get_default_directory(),
+            "data",
+            f"{self.feed_config.name}" + ext,
+        )
+        self.logger = logging.getLogger(
+            f"{self.__class__.__name__} - {feed_config.name}"
+        )
 
-    def __call__(self) -> List[Entry]:
-        return self.get_new_entries()
+    def __call__(self, save: bool = True) -> List[Entry]:
+        return self.get_new_entries(save=save)
 
-    def get_new_entries(self) -> List[Entry]:
+    def get_new_entries(self, save: bool = True) -> List[Entry]:
         entries: List[Entry] = list()
         if self.feed == self.local_feed:
             return entries
         for feed in self.feed:
             if feed not in self.local_feed:
                 entries.append(feed)
+        self.logger.debug(f"Found new {len(entries)} feeds")
+        if entries and save:
+            self.save_feed(self.feed)
+            self.logger.debug(f"Saved {len(entries)} as {self.local_file}")
         return entries
 
     @property
@@ -40,14 +52,12 @@ class FeedUpdater:
     def local_feed(self) -> Feed:
         if self._local_feed:
             return self._local_feed
+        if not os.path.isfile(self.local_file):
+            return Feed()
         feed_data = toml.load(self.local_file)
         self._local_feed = Feed(**feed_data)
         return self._local_feed
 
-    def _save_feed(self, feed: Feed):
+    def save_feed(self, feed: Feed):
         feed_data = attr.asdict(feed, recurse=True)
         save_as(feed_data, self.local_file)
-
-    @property
-    def local_file(self) -> str:
-        return os.path.join(self.config.config_dir, f"{self.feed_config.name}.toml")
--- telegram_rss/telegram.py
+++ telegram_rss/telegram.py
@@ -1,3 +1,4 @@
+import logging
 from telegram import Bot, ParseMode
 from time import sleep
 from typing import List
@@ -5,18 +6,21 @@ from typing import List
 from telegram_rss.config import Config
 from telegram_rss.feed import FeedUpdater
 
+logger = logging.getLogger(__name__)
+
 
 def send_message(bot: Bot, text: str, chat_ids: List[int]):
     for chat_id in chat_ids:
-        bot.send_message(chat_id, str, parse_mode=ParseMode.HTML)
+        bot.send_message(chat_id, str(text), parse_mode=ParseMode.HTML)
         sleep(0.05)
 
 
 def send_update(bot: Bot, config: Config):
     chat_ids = config.channels + config.users
     for feed_config in config.feeds:
-        updater = FeedUpdater(config, feed_config)
+        updater = FeedUpdater(feed_config)
         entries = updater.get_new_entries()
+        entries.reverse()
         for entry in entries:
             send_message(bot, str(entry), chat_ids)
         sleep(3.0)

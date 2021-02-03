0a8188bbf2ebfda4a3203a1d7e98dd229e616b78
hexatester <revolusi147id@gmail.com>
2021-02-03 19:50:03 +0700

    Add published on template

--- telegram_rss/config.py
+++ telegram_rss/config.py
@@ -14,6 +14,7 @@ logger = logging.getLogger(__name__)
 TEMPLATE_DATA = {
     "author": "Author",
     "source": "Source",
+    "published": "Published",
 }
 
 
--- telegram_rss/template.py
+++ telegram_rss/template.py
@@ -1,5 +1,6 @@
 TEMPLATE = """<a href="{{ entry.link }}">{{ entry.safe_title }}</a>
 <i>{{ author }}</i>: <b>{{ entry.author }}</b>
+{% if entry.published %}<i>{{ published }}</i>: <b>{{ entry.published }}</b>{% endif %}
 {{ entry.safe_description }}
 <i>{{ source }}</i>: <a href="{{ channel.link }}">{{ channel.safe_title }}</a>
 """
